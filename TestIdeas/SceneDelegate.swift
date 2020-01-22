d7d58e85387d6a102a864979b9b2919e5e62e780
Sylvan Ash <sylvan.d.ash@gmaill.com>
2020-01-22 19:51:54 +0300

    clean up the code

--- TestIdeas/SceneDelegate.swift
+++ TestIdeas/SceneDelegate.swift
@@ -14,14 +14,10 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
-        // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
-        // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         let windowScene: UIWindowScene = scene as! UIWindowScene
         self.window = UIWindow(windowScene: windowScene)
         self.window!.rootViewController = ResizeViewViewController()
         self.window!.makeKeyAndVisible()
-        //guard let _ = (scene as? UIWindowScene) else { return }
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
@@ -51,7 +47,4 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Use this method to save data, release shared resources, and store enough scene-specific state information
         // to restore the scene back to its current state.
     }
-
-
 }
-
